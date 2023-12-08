ecb49d0478e31112ce45002490b132c354a5e0ce
Andras Hejj <andras@nobank.one>
2023-12-08 18:38:02 +0100

    Update dependencies and remove unused code

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
 import "react-native-url-polyfill/auto";
 
-import { PierMpcSdkReactNativeProvider } from "@pier-wallet/mpc-lib/dist/package/react-native";
+import { PierMpcProvider } from "@pier-wallet/mpc-lib/dist/package/react-native";
 import React from "react";
 import Mpc from "./Mpc";
-import { PierServerVaultProvider } from "./pier-mpc-provider";
 
 export default function App() {
   return (
-    <PierMpcSdkReactNativeProvider verbose={true}>
-      <PierServerVaultProvider>
-        <Mpc />
-      </PierServerVaultProvider>
-    </PierMpcSdkReactNativeProvider>
+    <PierMpcProvider>
+      <Mpc />
+    </PierMpcProvider>
   );
 }
--- Mpc.tsx
+++ Mpc.tsx
@@ -1,71 +1,90 @@
 import { KeyShare, SessionKind } from "@pier-wallet/mpc-lib";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, SafeAreaView } from "react-native";
 import { useEffect, useState } from "react";
 import { PierMpcEthereumWallet } from "@pier-wallet/mpc-lib/dist/package/ethers-v5";
 import { ethers } from "ethers";
-import { usePierServerVault } from "./pier-mpc-provider";
-import { usePierMpcSdk } from "@pier-wallet/mpc-lib/dist/package/react-native";
+import { usePierMpc } from "@pier-wallet/mpc-lib/dist/package/react-native";
 
 // REMARK: Use should use your own ethers provider - this is just for demo purposes
 const ethereumProvider = new ethers.providers.JsonRpcProvider(
-  "https://rpc.sepolia.org"
+  "https://rpc.sepolia.org",
 );
 
 export default function Mpc() {
-  const pierMpcSdk = usePierMpcSdk();
-  const pierMpcVaultSdk = usePierServerVault();
+  const pierMpc = usePierMpc();
 
   const [keyShare, setKeyShare] = useState<KeyShare | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    (async () => {
+      await pierMpc.auth.signInWithPassword({
+        email: "mpc-lib-test@example.com",
+        password: "123456",
+      });
+    })();
+  }, [pierMpc]);
+
   const generateKeyShare = async () => {
     try {
       console.log("generating local key share...");
-      const tempKeyShare = await pierMpcVaultSdk.generateKeyShare();
+      const [mainKeyShare, backupKeyShare] =
+        await pierMpc.generateKeyShare2Of3();
 
-      console.log("local key share generated.", tempKeyShare.publicKey);
-      setKeyShare(tempKeyShare);
+      console.log("local key share generated.", mainKeyShare.publicKey);
+      // you should save backupKeyShare as well but we will focus only on the mainKeyShare in this tutorial
+      setKeyShare(mainKeyShare);
     } catch (e) {
       console.error(e);
     }
   };
 
   const [ethWallet, setEthWallet] = useState<PierMpcEthereumWallet | null>(
-    null
+    null,
   );
+  // const [btcWallet, setBtcWallet] = useState<PierMpcBitcoinWallet | null>(null);
+
   useEffect(() => {
     if (!keyShare) return;
+
     (async () => {
-      const signConnection = await pierMpcVaultSdk.establishConnection(
-        SessionKind.SIGN
+      const signConnection = await pierMpc.establishConnection(
+        SessionKind.SIGN,
+        keyShare.partiesParameters,
       );
       const ethWallet = new PierMpcEthereumWallet(
         keyShare,
         signConnection,
-        pierMpcVaultSdk,
-        ethereumProvider
+        pierMpc,
+        ethereumProvider,
       );
       setEthWallet(ethWallet);
+
+      // const btcWallet = new PierMpcBitcoinWallet(
+      //   keyShare,
+      //   PierMpcBitcoinWalletNetwork.Testnet,
+      //   signConnection,
+      //   pierMpc
+      // );
+      // setBtcWallet(btcWallet);
     })();
-  }, [keyShare]);
+  }, [keyShare, pierMpc]);
 
   const sendEthereumTransaction = async () => {
     if (!ethWallet) return;
 
     setIsLoading(true);
     try {
-      // send 1/10 of the balance to a zero address
+      // send 1/10 of the balance to the zero address
       const receiver = ethers.constants.AddressZero;
       const balance = await ethWallet.getBalance();
       const amountToSend = balance.div(10);
 
-      // create a transaction request
-      const txRequest = await ethWallet.populateTransaction({
+      // sign the transaction locally & send it to the network once we have the full signature
+      const tx = await ethWallet.sendTransaction({
         to: receiver,
         value: amountToSend,
       });
-      // sign the transaction locally & send it to the network once we have the full signature
-      const tx = await ethWallet.sendTransaction(txRequest);
       console.log("tx", tx.hash);
     } catch (e) {
       console.error(e);
@@ -73,21 +92,36 @@ export default function Mpc() {
       setIsLoading(false);
     }
   };
+  // const sendBitcoinTransaction = async () => {
+  //   if (!btcWallet) return;
+
+  //   setIsLoading(true);
+  //   try {
+  //     const receiver = 'tb1qw2c3lxufxqe2x9s4rdzh65tpf4d7fssjgh8nv6'; // testnet faucet
+  //     const amountToSend = 800n; // 0.00000800 BTC = 800 satoshi
+  //     const feePerByte = 1n; // use a fee provider to get a more accurate fee estimate - otherwise check minimum fee manually
+
+  //     // create a transaction request
+  //     const txRequest = await btcWallet.populateTransaction({
+  //       to: receiver,
+  //       value: amountToSend,
+  //       feePerByte,
+  //     });
+
+  //     // sign the transaction locally & send it to the network once we have the full signature
+  //     const tx = await btcWallet.sendTransaction(txRequest);
+  //     console.log('tx', tx.hash);
+  //   } catch (e) {
+  //     console.error(e);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
 
   return (
     <>
+      <SafeAreaView />
       <View style={{ flex: 1, padding: 20 }}>
-        <Button
-          title="Sign in as Test (fix)!"
-          onPress={async () => {
-            await pierMpcSdk.auth.signInWithPassword({
-              email: "mpc-lib-test@example.com",
-              password: "123456",
-            });
-
-            console.log("signed in as test user");
-          }}
-        />
         <Button
           title="Generate Key Share yay"
           onPress={generateKeyShare}
@@ -101,6 +135,7 @@ export default function Mpc() {
           disabled={isLoading}
         />
       </View>
+      <SafeAreaView />
     </>
   );
 }
