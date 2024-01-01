f44cea164afa8dd089c3261f5a6dffa63b1ffb4e
Andras Hejj <andras@nobank.one>
2024-01-01 13:19:26 +0100

    Remove unused code and dependencies

--- App.tsx
+++ App.tsx
@@ -1,23 +1,8 @@
 import { PierMpcProvider } from "@pier-wallet/mpc-lib/dist/package/react-native";
-import React, { useEffect } from "react";
+import React from "react";
 import Mpc from "./Mpc";
-import { Text } from "react-native";
-import { useGoogleLogin } from "./useGoogleLogin";
 
 export default function App() {
-  // const { signIn, signedIn } = useGoogleLogin();
-  // useEffect(() => {
-  //   (async () => {
-  //     if (!signedIn) {
-  //       await signIn();
-  //     }
-  //   })();
-  // }, [signedIn]);
-
-  // if (!signedIn) {
-  //   return <Text>Signing in...</Text>;
-  // }
-
   return (
     <PierMpcProvider>
       <Mpc />
